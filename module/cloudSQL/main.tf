440ff81bf2672a54d474c6afb82894777d177478
Maxcwt <148951047+Maxcwt@users.noreply.github.com>
2025-03-31 19:51:00 +0200

    fix: [AIC-04] fix cloudsql module

--- module/cloudSQL/main.tf
+++ module/cloudSQL/main.tf
@@ -6,10 +6,10 @@ resource "google_sql_database_instance" "main" {
   settings {
     tier = var.db_instance_tier
 
-    availability_type = var.db_availability_type //"ZONAL"
-    disk_size         = var.db_disk_size // disk size 20GB
-    disk_type         = var.db_disk_type//"PD_SSD"
-    disk_autoresize   = var.db_disk_autoresize // false
+    availability_type = var.db_availability_type
+    disk_size         = var.db_disk_size
+    disk_type         = var.db_disk_type
+    disk_autoresize   = var.db_disk_autoresize
 
     ip_configuration {
       ipv4_enabled    = false
